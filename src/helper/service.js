b3c060e01916e9d1abec4faf734dea5d639c8508
Kirara Bernstein <fahtul.singaparna@gmail.com>
2024-11-08 10:07:04 +0700

    edit: request headers

--- src/helper/service.js
+++ src/helper/service.js
@@ -4,7 +4,11 @@ const axios = require('axios')
 const Service = {
     fetchService: async (url) => {
         try {
-            return await axios.get(url);
+            return await axios.get(url,{
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
+                }
+            });
         } catch (e) {
             return {
                 status: e.response ? e.response.status : 500,
