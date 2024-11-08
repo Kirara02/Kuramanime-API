707adcfce0b6a24a63cb407df8ca4408dc767022
Kirara Bernstein <fahtul.singaparna@gmail.com>
2024-11-08 10:22:26 +0700

    edit: request headers

--- src/helper/service.js
+++ src/helper/service.js
@@ -6,7 +6,11 @@ const Service = {
         try {
             return await axios.get(url,{
                 headers: {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
+                    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Mobile Safari/537.36',
+                    'Accept': 'application/json',
+                    // 'Accept-Encoding': 'gzip, deflate, br, zstd',
+                    'Accept-Language': 'en-US,en;q=0.9,ja;q=0.8,id;q=0.7,ru;q=0.6',
+                    'Cache-Control': 'no-cache',
                 }
             });
         } catch (e) {
